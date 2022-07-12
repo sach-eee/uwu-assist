ea4151cc68ca172105d14c80357885e35dc911ca
sach-eee <sachirakarunarathna029@gmail.com>
2022-07-12 10:26:32 +0530

    login and signup

--- Login and sign up/login&signup.php
+++ Login and sign up/login&signup.php
@@ -2,25 +2,39 @@
 
 session_start();
 
-$con=mysqli_connect('localhost','root','emsUwu@ss1st');
+$con=mysqli_connect('localhost','root','');
 
-mysqli_select_db('uwuassist')
+mysqli_select_db($con, 'uwuassist');
 
 $email = $_POST['email'];
 $password = $_POST['password']; 
 
-$s ="select * from login_info where Email ='$email'";
 
-$result= mysqli_query($con,$s);
+
+$s ="select * from login_info where email ='$email'";
+
+// echo $s;
+
+$result= mysqli_query($con, $s);
 $num= mysqli_num_rows($result);
 
-if($num==1){
 
-    echo"Username Already Taken";
+if($num==1){
+    echo "Have account:";
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+
+        // If you want to display all results from the query at once:
+        // print_r($row);
+    
+        // If you want to display the results one by one
+        echo $row['username'];
+        echo $row['password']; // etc..
+    
+    }
+    
 }else{
-    $log="insert into login_info(Email,Password)values('$email' ,'$password')";
-    mysqli_query($con,$log);
-    echo
+
+    echo "no account.";
 }
 
 ?>
--- Login and sign up/signup.php
+++ Login and sign up/signup.php
@@ -1,17 +1,32 @@
 <?php
 
 session_start();
-header('location:login.php');
+// header('location:login.php');
 
-$con=mysqli_connect('localhost','root','emsUwu@ss1st');
+//trigger exception in a "try" block
+try {
+    $con=mysqli_connect('localhost','root','');
+    //If the exception is thrown, this text will not be shown
+    echo 'If you see this, the number is 1 or below';
+  }
+  
+  //catch exception
+  catch(Exception $e) {
+    echo 'Message: ' .$e->getMessage();
+  }
+  
 
-mysqli_select_db('uwuassist');
+
+
+mysqli_select_db($con, 'uwuassist');
 
 $name  = $_POST['user'];
 $email = $_POST['email'];
 $password = $_POST['password']; 
 
-$s ="select * from login_info where Name ='$name'";
+$s ="select * from login_info where username ='$name'";
+
+
 
 $result= mysqli_query($con,$s);
 $num= mysqli_num_rows($result);
@@ -20,9 +35,15 @@ if($num==1){
 
     echo"Username Already Taken";
 }else{
-    $log="insert into login_info(Name,Email,Password)values('$name','$email' ,'$password')";
-    mysqli_query($con,$log);
-    echo "Registration successfull";
-}
 
+    try {
+        $log="insert into login_info values ('$name', '$email', '$password');";
+        mysqli_query($con,$log);
+        echo "Registration successfull";
+    }  
+    //catch exception
+    catch(Exception $e) {
+        echo 'Message: ' .$e->getMessage();
+    }
+}   
 ?>
